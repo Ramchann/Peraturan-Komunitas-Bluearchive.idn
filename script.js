c4d23cb83fb163eda38a29e4dc3491857a82aa33
Muhammad Ramdhani Husnil Mubarak <134475978+Ramchann@users.noreply.github.com>
2025-01-29 12:41:26 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const agreeCheckbox = document.getElementById('agreeCheckbox');
+const agreeButton = document.getElementById('agreeButton');
+
+agreeCheckbox.addEventListener('change', () => {
+    agreeButton.disabled = !agreeCheckbox.checked;
+});
+
+agreeButton.addEventListener('click', () => {
+    // Redirect ke grup wa
+    window.location.href = 'https://chat.whatsapp.com/F15145UCF950PLm6RswMHk';
+});
